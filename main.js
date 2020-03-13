37ad8b4c89bf91b032fd8e233504656169bc7de8
Yosra <yos.skhiri@gmail.com>
2020-03-13 16:26:43 +0100

    added the search filter

---
+++ main.js
@@ -0,0 +1,16 @@
+let mames;
+cards = document.querySelectorAll('.card');
+
+function search() {
+  let input;
+  input = document.getElementById('search').value.toLowerCase();
+  
+  for (i = 0; i < cards.length; i++) {
+    a = cards[i].children[1].innerText.toLowerCase();
+    if (a.indexOf(input) > -1 || input === "") {
+        cards[i].style.display = "";
+    } else {
+        cards[i].style.display = "none";
+    }
+  }
+}

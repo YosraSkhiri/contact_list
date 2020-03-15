93b1527659beffb559837dd33f989e6755914ecb
Yosra <yos.skhiri@gmail.com>
2020-03-15 13:41:31 +0100

    added a debounce function for a better performance

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-let mames;
+let cards;
 cards = document.querySelectorAll('.card');
 
-function search() {
+let search = debounce(function () {
   let input;
   input = document.getElementById('search').value.toLowerCase();
-  
+
   for (i = 0; i < cards.length; i++) {
     a = cards[i].children[1].innerText.toLowerCase();
     if (a.indexOf(input) > -1) {
-        cards[i].style.display = "";
+      cards[i].style.display = "";
     } else {
-        cards[i].style.display = "none";
+      cards[i].style.display = "none";
     }
   }
-}
+
+}, 250);
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
